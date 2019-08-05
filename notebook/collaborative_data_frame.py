a2cfa4fdde4cfa6cdd7f176cbbeecf85b66cab80
Mashaal Musleh <meshalawy@gmail.com>
2019-08-05 15:03:10 +0300

    Dockerizing the notebook with collaborative cleaning support.

--- notebook/collaborative_data_frame.py
+++ notebook/collaborative_data_frame.py
@@ -14,6 +14,7 @@ import math
 import collections
 import threading
 import time
+import os
 
 from functools import reduce
 
@@ -23,7 +24,7 @@ from ipywidgets.widgets import Button, VBox, HBox
 class CollaborativeDataFrame(pd.DataFrame):
     def __init__(self, data, *args, **kwargs):
         url = None
-        hostname = kwargs.get('hostname', '127.0.0.1')
+        hostname = kwargs.get('hostname', os.environ.get('DB', '127.0.0.1'))
         metadata = kwargs.get('metadata', None)
         db_client = None
 
